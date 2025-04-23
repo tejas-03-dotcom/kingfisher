f13302377f2a4801416ffd10dedf2a039421d1e8
tejas-03-dotcom <141627026+tejas-03-dotcom@users.noreply.github.com>
2025-04-24 03:05:24 +0530

    Update 12marchServer.js

--- 12marchServer.js
+++ 12marchServer.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '12march_withmaggi.html'));
 });
 
 //extra code, may need at some point - 22 April
